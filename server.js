5202eddb5b636bb1519d35e1cb9ba6268b82acbd
Sunny0969 <f190969@nu.edu.pk>
2022-04-14 22:38:51 +0500

    Registered successfully with authentication

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
+const bcrypt = require('bcrypt')
+const users=[]
+
 app.set('view-engine','ejs')
 app.use(express.urlencoded({extended: false}))
 
@@ -17,6 +20,18 @@ app.get('/register',(req,res)=>{
     res.render('register.ejs')
 })
 app.post('/register',(req,res)=>{
-req.body.email
+try{
+    const hashedPassword =  bcrypt.hash(req.body.password,10)
+    users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    })
+    res.redirect('/login')
+}catch{
+    res.redirect('/register')
+}
+    console.log(users)
 })
 app.listen(3000)
