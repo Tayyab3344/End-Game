5d8065e8f609bbfcea8b2ee8c0ed092b6334f16a
Sunny0969 <f190969@nu.edu.pk>
2022-04-28 11:10:43 +0500

    Authentication Completed

--- passport-config.js
+++ passport-config.js
@@ -1,4 +1,26 @@
-const LocalStratrgy = require('passport-local').Strategy
-function initialize(passport){
-passport.use(new LocalStrategy({usernameField: 'email'}))
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail){
+    const autheticateUser = async (email, password, done)=>{
+    const user = getUserByEmail(email)
+    if(user == null){
+        return done(null,false,{message: 'No user with this Email'})
+        }
+        try{
+            if(await  bcrypt.compare(password,user.password)){
+                return done(null,user)
+            }else{
+                return done(null,false,{message:'Password incorrect'})
+            }
+        }catch(e){
+            return done(e)
+        }
+    }
+passport.use(new LocalStrategy({usernameField: 'email'},autheticateUser))
+passport.serializeUser((user,done) => done(null,user.id))
+passport.deserializeUser((id,done) =>{
+    return done(null,getUserById(id))
+})
 }
+module.exports = initialize
--- server.js
+++ server.js
@@ -1,29 +1,49 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+ }
 const express = require('express')
-const res = require('express/lib/response')
+//const res = require('express/lib/response')
 const app = express()
+const passport = require('passport')
 const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+
 const initializePassport = require('./passport-config')
-initializePassport(passport)
+initializePassport(passport, email => users.find(user => user.email === email), id => users.find(user => user.id === id))
+
 const users=[]
 
 app.set('view-engine','ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false, 
+    saveUninitialized: false  }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
+app.get('/', checkAuthenticated, (req,res)=>{
+    req.render('./index.ejs',{ name: req.user.name})
 })
-app.get('/Login',(req,res)=>{
+app.get('/Login',checkNotAuthenticated, (req,res)=>{
     res.render('login.ejs')
 })
-app.post('/login',(req,res)=>{
-
-})
-app.get('/register',(req,res)=>{
+app.post('/login',checkNotAuthenticated, passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+app.get('/register',checkNotAuthenticated,(req,res)=>{
     res.render('register.ejs')
 })
-app.post('/register',(req,res)=>{
+app.post('/register',checkNotAuthenticated,async (req,res)=>{
 try{
-    const hashedPassword =  bcrypt.hash(req.body.password,10)
+    const hashedPassword = await bcrypt.hash(req.body.password,10)
     users.push({
         id: Date.now().toString(),
         name: req.body.name,
@@ -34,6 +54,26 @@ try{
 }catch{
     res.redirect('/register')
 }
-    console.log(users)
+    //console.log(users)
 })
+
+app.delete('/logout',(req,res)=>{
+    req.logOut()
+    res.redirect('/login')
+})
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+      return res.redirect('/')
+    }
+    next()
+
+}
 app.listen(3000)
