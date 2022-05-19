4ae5d42a7a3f9859a0cb52e9119b88f42f54ea8e
Tayyab3344 <90280096+Tayyab3344@users.noreply.github.com>
2022-05-19 23:02:05 +0500

    Profile Screen Created with Routing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
- }
+  require('dotenv').config()
+}
 const express = require('express')
 const app = express()
 const passport = require('passport')
@@ -10,8 +10,8 @@ const session = require('express-session')
 
 
 // New
-require('./views/configuration/passport-config')(passport);
-  
+require('./passport-config')(passport);
+
 app.use(require('serve-static')(__dirname + '/../../public'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
@@ -23,11 +23,11 @@ app.use(passport.session());
 app.use("/views",express.static(__dirname + "/views"))
 const methodOverride = require('method-override')
 
-const initializePassport = require('./views/configuration/passport-config')
+const initializePassport = require('./passport-config')
 initializePassport(
-    passport, 
-    email => users.mysignup(user => user.email === email),
-    id => users.mysignup(user => user.id === id))
+  passport, 
+  email => users.mysignup(user => user.email === email),
+  id => users.mysignup(user => user.id === id))
 
 const users=[]
 
@@ -35,9 +35,9 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(flash())
 app.use(session({
-    secret: process.env.SESSION_SECRET, 
-    resave: false, 
-    saveUninitialized: false  }))
+  secret: process.env.SESSION_SECRET, 
+  resave: false, 
+  saveUninitialized: false  }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
@@ -45,7 +45,7 @@ app.use(methodOverride('_method'))
 // -- DataBase Work
 const mongoose=require('mongoose');
 const url='mongodb+srv://TechBloggers:techbloggers123@cluster0.i1ic8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const MySignup=require("C:/Users/ILYAS/Desktop/Web-Project/End-Game/Model/schema.js")  
+const MySignup=require("C:/Users/Ocean Computers/Desktop/WEB Engineering/End-Game/Model/schema.js")  
 mongoose.connect(url)
 .then((result)=>console.log('connected to db'))
 .catch((err)=>console.log(err))
@@ -53,102 +53,98 @@ mongoose.connect(url)
 // -- End --
 
 app.get('/', checkAuthenticated, (req,res)=>{
-    res.render('MainScreen.ejs')
+  res.render('MainScreen.ejs')
 })
 app.get('/Login', (req,res)=>{
-    res.render('login')
+  res.render('login')
 })
 
 app.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true
-    })(req, res, next);
-  });
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
+});
 
-  app.get('/forgot',checkNotAuthenticated,(req,res)=>{
-    res.render('forgot.ejs');
-  });
-
-  app.post('/forgot',checkNotAuthenticated,(req,res)=>{
-    res.render('forgot.ejs')
-})
-
-  app.get('/blogs',checkNotAuthenticated,(req,res)=>{
-    res.render('blogs.ejs');
 
 //
-  app.get('/MainScreen',checkNotAuthenticated,(req,res)=>{
-    res.render('MainScreen.ejs');
+app.get('/MainScreen',checkNotAuthenticated,(req,res)=>{
+  res.render('MainScreen.ejs');
 });
 app.post('/Dashboard',checkNotAuthenticated,async (req,res)=>{
-    const AddBlogs = new AddBlogs({
-        Name: req.body.Name,
-        URL: req.body.URL,
-        subject: req.body.subject,
-        message: req.body.message,
-        PicImage: req.body.PicImage,
-       });
-       AddBlogs.save()
-       .then((result)=>{res.send(result)})
-       .catch((err)=>{
-         console.log(err)
-       });
+  const AddBlogs = new AddBlogs({
+      Name: req.body.Name,
+      URL: req.body.URL,
+      subject: req.body.subject,
+      message: req.body.message,
+      PicImage: req.body.PicImage,
+     });
+     AddBlogs.save()
+     .then((result)=>{res.send(result)})
+     .catch((err)=>{
+       console.log(err)
+     });
 })
 
-
-//
-  app.get('/Blogs',checkAuthenticated,(req,res)=>{
-    res.render('blogs');
+app.get('/Blogs',checkAuthenticated,(req,res)=>{
+  res.render('blogs');
 });
 
 app.post('/Blogs',checkNotAuthenticated,(req,res)=>{
-    res.render('blogs');
+  res.render('blogs');
+});
+
+app.get('/profile',checkAuthenticated,(req,res)=>{
+  res.render('profile');
+});
+
+app.post('/profile',checkNotAuthenticated,(req,res)=>{
+  res.render('profile');
 });
 
 app.get('/register',checkNotAuthenticated,(req,res)=>{
-    res.render('register.ejs')
+  res.render('register.ejs')
 })
 app.post('/register',checkNotAuthenticated,async (req,res)=>{
 try{
-    const hashedPassword = await bcrypt.hash(req.body.password,10)
-   
-    // -- DataBase 
-    const mysignup=new MySignup({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-       });
-       mysignup.save()
-       .then((result)=>{res.send(result)})
-       .catch((err)=>{
-         console.log(err)
-       });
-       // -- End 
-    res.redirect('/login')
+  const hashedPassword = await bcrypt.hash(req.body.password,10)
+ 
+  // -- DataBase 
+  const mysignup=new MySignup({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+     });
+     mysignup.save()
+     .then((result)=>{res.send(result)})
+     .catch((err)=>{
+       console.log(err)
+     });
+     // -- End 
+  res.redirect('/login')
 }catch{
-    res.redirect('/register')
+  res.redirect('/register')
 }
-    console.log(users)
+  console.log(users)
 })
 
 app.delete('/logout',(req,res)=>{
-    req.logOut()
-    res.redirect('/login')
+  req.logOut()
+  res.redirect('/login')
 })
 function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }   
-    res.redirect('/login')
+  if(req.isAuthenticated()){
+      return next()
+  }   
+  res.redirect('/login')
 }
 function checkNotAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-      return res.redirect('/')
-    }
-    next()
+  if(req.isAuthenticated()){
+    return res.redirect('/')
+  }
+  next()
 
 }
 app.listen(3000)
